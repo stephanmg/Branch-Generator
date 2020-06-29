1aa5b4710a1014c8a22f9364ceacab9e6226fbb4
Stephan Grein <stephan@syntaktischer-zucker.de>
2020-06-29 11:03:52 +0200

    Script improvements.

--- scripts/build_ys.sh
+++ scripts/build_ys.sh
@@ -9,7 +9,7 @@ for file in *swc; do
     echo "filename: $FILENAME"
     echo -n "Processing file $file now..."
     "$BINARY" -call "create_two_way_branch_from_swc(\"$file\", \"$ER_SCALE_FACTOR\", \"$NUMREFS\")" &> /dev/null
-    mv imported_y_structure.ugx "${FILENAME}_ref_0.ugx"
+    mv "imported_y_structure.ugx" "${FILENAME}_ref_0.ugx"
     for i in $(seq 1 $NUMREFS); do
       mv "imported_y_structure_refined_${i}.ugx" "${FILENAME}_ref_${i}.ugx"
     done
